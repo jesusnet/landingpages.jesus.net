53ef50b24c4ba51f18feba16cad2023d0864e6ff
Paweł Fober <pfober@wiredot.com>
2014-05-26 09:58:51 +0200

    text on image positioning

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	middle_position();
+	valign_position();
 });
 
 $(window).resize(function() {
-	middle_position();
+	valign_position();
 });
 
-function middle_position() {
+function valign_position() {
 	
 	var img_height = $('.main-image img').height();
 	var txt_height = $('.main-image .txt').height();
-	var padding_top = (img_height - txt_height)/2;
+
+	var middle = (img_height - txt_height)/2;
 	var bottom = img_height - txt_height - 20;
 
-	//alert(bottom);
 
-	$('.main-image .txt .middle').css('padding-top', padding_top);
+	$('.main-image .txt .middle').css('padding-top', middle);
 	$('.main-image .txt .bottom').css('padding-top', bottom);
 }
